7b05637c1c724d1fb0d00163698cb448da073460
harshal211997 <harshal211997@gmail.com>
2024-02-17 11:09:00 +0530

    middleware changes

--- models/tourModels.js
+++ models/tourModels.js
@@ -1,4 +1,5 @@
 const Mongoose =  require("mongoose")
+const slugify = require('slugify')
 
 //schema declaration: dcocument structure
 const tourSchema = new Mongoose.Schema({
@@ -8,6 +9,7 @@ const tourSchema = new Mongoose.Schema({
         unique:true,
         trim: true
     },
+    slug: String,
     duration:{
         type:Number,
         required:[true, 'A Tour Must have duration']
@@ -43,7 +45,7 @@ const tourSchema = new Mongoose.Schema({
     description:{
         type: String,
         trim: true,
-        required: [true, 'A tour must have descripion']
+       // required: [true, 'A tour must have descripion']
     },
     //image name (string), will put just imae name in DB and store the image in fileSystem
     imageCover:{
@@ -63,9 +65,66 @@ const tourSchema = new Mongoose.Schema({
     startDates: {
         type: [Date],
 
+    },
+    //for query middleware
+    secrateTour : {
+        type: Boolean,
+        default: false
     }
+},
+//schema options to get virtual property out 
+{
+    toJSON: { virtuals: true },
+    toObject : { virtuals : true}
 });
+
+//virtual property: to set some property modified which outside a database
+//need to use regular function insted of arrow because arrow function does not have this keyword
+tourSchema.virtual('durationWeeks').get(function() {
+    return this.duration / 7;
+})
+
+//Document Middleware is monggose middleware: runs only for .save() and .create() command
+//to see any document save before DB we will use pre Document middleware
+tourSchema.pre('save',function(next){
+   // console.log(this);
+   this.slug = slugify(this.name, {lower: true});
+   next();
+})
+// //we can able to call multiple middleware
+// tourSchema.pre('save', function(next){
+//     console.log('Will Save Document...');
+//     next();
+// })
+// //Post Document middleware
+// tourSchema.post('save', function(doc, next){
+//     console.log(doc);
+//     next();
+// })
+
+//Query middleware: before or after any query run will see output in query only no change in DB
+//this keyword point to current query
+//this will run find() method from controller and before query end this middleware will run and it will
+//remove super secrate tour
+tourSchema.pre('find', function(next) {
+    this.find({secrateTour : {$ne: true}});
+    this.start = Date.now();
+    next();
+})
+//for find one document
+tourSchema.pre('findOne',function(next){
+    this.findOne({secrateTour: {$ne: true}});
+    this.start = Date.now();
+    next();
+})
+tourSchema.post(/^find/, function(doc, next){
+    console.log(`Query Took ${Date.now() - this.start} milliseconds!`);
+    console.log(doc);
+    next();
+})
 //Colletion
 const Tour = Mongoose.model('Tour', tourSchema)
 
 module.exports  = Tour;
+
+
